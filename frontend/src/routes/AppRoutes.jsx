3e20fefad608e49ed0e7b591780899c486d4485d
Prasad Gaikwad <prasad.gaikwad054@gmail.com>
2026-08-21 01:19:18 +0530

    deleted unnecessary pages

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -7,9 +7,6 @@ import Services from "../pages/Services";
 import CollectionsPage from "../pages/Collections";
 import VerifyEmail from "../pages/VerifyEmail";
 import WatchDetails from "../pages/WatchDetails";
-import Products from "../pages/Products";
-import ProductDetails from "../pages/ProductDetails";
-import Cart from "../pages/Cart";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Account from "../pages/Account";
@@ -28,9 +25,6 @@ function AppRoutes() {
           <Route path="/collections" element={<CollectionsPage />} />
           <Route path="/collections/:reference" element={<WatchDetails />} />
           <Route path="/verify-email/:token" element={<VerifyEmail />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/product/:id" element={<ProductDetails />} />
-          <Route path="/cart" element={<Cart />} />
           <Route path="/point-of-sale" element={<PointOfSale />} />
           <Route path="/appointment-confirmation" element={<AppointmentConfirmation />} />
         </Route>
